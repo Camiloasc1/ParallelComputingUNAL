0ef77de6c5bc7d7ccd6e1b6f4a2d0339db538005
caasanchezcu <caasanchezcu@unal.edu.co>
2016-04-08 15:48:05 -0500

    PIFork with variable number of processes.

--- PIFork.cpp
+++ PIFork.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <unistd.h>
 
-#define LIMIT (unsigned int) 1E9
+#define LIMIT (unsigned long) 1E10
+#define PROCESSES 8
 
 using namespace std;
 
 struct Task {
-    unsigned int start;
-    unsigned int end;
+    unsigned long start;
+    unsigned long end;
     double res;
 };
 
 void *PartialLeibniz(void *task) {
     Task *myTask = (Task *) task;
     myTask->res = 0.0;
-    for (auto i = myTask->start; i < myTask->end; i++) {
+    for (auto i = myTask->start; i < myTask->end; ++i) {
         if (i % 2 == 0)
             myTask->res += 1.0 / ((i << 1) + 1);
         else
@@ -31,28 +32,39 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child
-        Task myTask;
-        read(taskpipe[0], &myTask, sizeof(Task));
-        PartialLeibniz(&myTask);
-        write(respipe[1], &myTask, sizeof(Task));
-    }
-    else if (pid > 0) {
-        // Parent
+    for (auto i = 0u; i < PROCESSES; ++i) {
         Task task;
-        task.start = 0;
-        task.end = LIMIT;
+        task.start = LIMIT / PROCESSES * i;
+        task.end = LIMIT / PROCESSES * (i + 1);
         write(taskpipe[1], &task, sizeof(Task));
-        read(respipe[0], &task, sizeof(Task));
-        cout << "Sum\t" << task.res << endl;
-        cout << "PI\t" << task.res * 4.0 << endl;
     }
-    else {
-        // Error
-        cerr << "fork() failed" << endl;
-        return EXIT_FAILURE;
+
+    for (auto i = 0u; i < PROCESSES; ++i) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child
+            Task myTask;
+            read(taskpipe[0], &myTask, sizeof(Task));
+            PartialLeibniz(&myTask);
+            write(respipe[1], &myTask, sizeof(Task));
+            return EXIT_SUCCESS;
+        }
+        else if (pid > 0) {
+            // Parent
+        }
+        else {
+            // Error
+            cerr << "fork() failed" << endl;
+            return EXIT_FAILURE;
+        }
     }
+    double sum = 0.0;
+    for (auto i = 0u; i < PROCESSES; ++i) {
+        Task res;
+        read(respipe[0], &res, sizeof(Task));
+        sum += res.res;
+    }
+    cout << "Sum\t" << sum << endl;
+    cout << "PI\t" << sum * 4.0 << endl;
     return EXIT_SUCCESS;
 }
